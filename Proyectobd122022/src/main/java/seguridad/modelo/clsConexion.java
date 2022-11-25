660cc99e40eb780f0a44289b9f3e31b310abeb11
Juan Sandoval <jsandovalc9@miumg.edu.gt>
2022-11-25 17:43:30 -0600

    Proyecto Principal

--- Proyectobd122022/src/main/java/seguridad/modelo/clsConexion.java
+++ Proyectobd122022/src/main/java/seguridad/modelo/clsConexion.java
@@ -4,10 +4,10 @@ import java.sql.*;
 
 public class clsConexion {
 
-  private static final String JDBC_URL = "jdbc:mysql://localhost/bdexamenfinal?useSSL=false&serverTimezone=UTC";  
+  private static final String JDBC_URL = "jdbc:mysql://localhost/examenfinalbd?useSSL=false&serverTimezone=UTC";  
 
     private static final String JDBC_USER = "root";
-    private static final String JDBC_PASS = "123456789";
+    private static final String JDBC_PASS = "admin123";
 
    
     public static Connection getConnection() throws SQLException {
